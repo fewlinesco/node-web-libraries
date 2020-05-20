59aa1fe2a386b6a03faf48b91d57d190828752f4
Anthony Montaigne <fuzznimp@anthonys-mbp.home>
2020-05-20 10:10:55 +0200

    added migrations for tests

--- packages/migration/test/migrations/20200511073348-create-profiles.sql
+++ packages/migration/test/migrations/20200511073348-create-profiles.sql
@@ -1,11 +1,9 @@
 CREATE TABLE "profiles" (
   "id" uuid NOT NULL,
-  "user_id" uuid NOT NULL,
   "first_name" varchar(255),
   "lase_name" varchar(255),
   "avatar_url" varchar(255),
   "created_at" timestamp NOT NULL DEFAULT NOW(),
   "updated_at" timestamp NOT NULL DEFAULT NOW(),
-  FOREIGN KEY ("user_id") REFERENCES "users"("id")
   PRIMARY KEY ("id")
 );
