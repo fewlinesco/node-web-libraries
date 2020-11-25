89e5d6d5088d06c8f78d6057f1cbe3999411bdbf
Julien Leclercq <julien.leclercq@gmx.fr>
2020-11-25 17:32:52 +0100

    try comment to find bug

--- bin/build-and-push.sh
+++ bin/build-and-push.sh
@@ -37,7 +37,7 @@ docker build \
 		--tag ${imageName}:${releaseName}-latest \
 		.
 
-docker push $dockerImage
-docker push $dockerImageLatest
+# docker push ${dockerImage}
+# docker push ${dockerImageLatest}
 
 # _updateManifest manifest.txt connect-demo-signup $environment $dockerTag
