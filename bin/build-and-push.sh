0f86809474b942043feeefeac772ca8affca83e6
Julien Leclercq <julien.leclercq@gmx.fr>
2020-11-25 16:38:07 +0100

    add short commit

--- bin/build-and-push.sh
+++ bin/build-and-push.sh
@@ -12,11 +12,12 @@ gitRepo=${2:?"git repository URL is required as second argument"};
 gitCommit=${4:?"git commit SHA is required as fourth argument"};
 gitBranch=${3:?"git branch is required as third argument"};
 
+gitShortCommit=$(echo $CIRCLE_SHA1 | cut -c -7)
 releaseName=$(echo ${gitBranch} | sed 's/^\(CU-[[:alnum:]]*\).*/\1/')
 
 environment=$(_appEnvironment $gitBranch)
 dockerTag=${environment}_${gitCommit}
-dockerImage=$(echo "fewlines/${imageName}: ${imageName}:${releaseName}-${GIT_SHORT_SHA}")
+dockerImage=$(echo "fewlines/${imageName}: ${imageName}:${releaseName}-${gitShortCommit}")
 dockerImageLatest=$(echo "fewlines/${imageName}:$(_appEnvironment $branch)_latest")
 
 docker build \
