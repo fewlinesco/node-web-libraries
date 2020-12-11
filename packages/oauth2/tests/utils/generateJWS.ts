54e8e0d776a34ea1f98012e4bf7e1c0468b1e6c3
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2020-12-11 09:32:09 +0100

    Added generateJWS + some default object (#58)

---
+++ packages/oauth2/tests/utils/generateJWS.ts
@@ -0,0 +1,33 @@
+import type { CustomPayload } from "@src/types";
+import {
+  defaultAsymmetricAlgoKeyPair,
+  defaultPayload,
+  defaultSecret,
+} from "@tests/utils";
+import jwt from "jsonwebtoken";
+
+export function generateHS256JWS(
+  customPayload?: CustomPayload,
+  secret?: string,
+): string {
+  return jwt.sign(
+    customPayload ? customPayload : defaultPayload,
+    secret ? secret : defaultSecret,
+    {
+      algorithm: "HS256",
+    },
+  );
+}
+
+export function generateRS256JWS(
+  customPayload?: CustomPayload,
+  privateKey?: string,
+): string {
+  return jwt.sign(
+    customPayload ? customPayload : defaultPayload,
+    privateKey ? privateKey : defaultAsymmetricAlgoKeyPair.privateKey,
+    {
+      algorithm: "RS256",
+    },
+  );
+}
