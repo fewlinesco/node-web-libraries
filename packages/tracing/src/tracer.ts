0d950f90cdfa0ef4f7cdd9ce345d73dc0df1fb62
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-05-26 16:54:36 +0200

    Trying to use NodeTracerProvider

--- packages/tracing/src/tracer.ts
+++ packages/tracing/src/tracer.ts
@@ -13,15 +13,17 @@ import {
 import { AsyncHooksContextManager } from "@opentelemetry/context-async-hooks";
 import { CollectorTraceExporter } from "@opentelemetry/exporter-collector";
 // import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { NodeTracerProvider } from "@opentelemetry/node";
+// import { NodeTracerProvider } from "@opentelemetry/node";
 import {
-  // BasicTracerProvider,
+  BasicTracerProvider,
   SimpleSpanProcessor,
 } from "@opentelemetry/tracing";
 
 import type { TracingConfig } from "./config";
 
-const provider = new NodeTracerProvider();
+const provider = new BasicTracerProvider();
+
+// const provider = new NodeTracerProvider();
 // registerInstrumentations({
 //   logLevel: DiagLogLevel.INFO,
 //   plugins: {
