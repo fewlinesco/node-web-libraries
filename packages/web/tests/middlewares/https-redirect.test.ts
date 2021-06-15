b730bbd95fab1edbdd9fefb6982a8a71259e3a19
meyer clement <meyclem@gmail.com>
2021-06-15 13:33:12 +0200

    add tests

--- packages/web/tests/middlewares/https-redirect.test.ts
+++ packages/web/tests/middlewares/https-redirect.test.ts
@@ -7,8 +7,41 @@ import {
   errorMiddleware,
   httpsRedirectMiddleware,
 } from "../../src/middlewares";
+import { readBody } from "../../src/utils";
 
-test("catch the error when a WebError is thrown and update the status code and the body accordingly", async () => {
+test("Redirects to https with permanent redirect code when host is not localhost and specified scheme is http", async () => {
+  expect.assertions(2);
+
+  const tracer = new InMemoryTracer();
+  const middleware = errorMiddleware(tracer);
+  const handler = (): void => {
+    response.end("OK");
+  };
+  const wrappedhandler = wrapMiddlewares(
+    [httpsRedirectMiddleware(tracer), middleware],
+    handler,
+  );
+
+  const host = "test.test";
+  const url = "/test?query=string";
+  const response = new httpMock.Response();
+
+  await wrappedhandler(
+    new httpMock.Request({
+      url,
+      headers: {
+        "x-forwarded-proto": "http",
+        host,
+      },
+    }),
+    response,
+  );
+
+  expect(response.statusCode).toBe(HttpStatus.PERMANENT_REDIRECT);
+  expect(response.getHeader("Location")).toBe(`https://${host}${url}`);
+});
+
+test("Do not redirect when the specified scheme is https", async () => {
   expect.assertions(1);
 
   const tracer = new InMemoryTracer();
@@ -20,20 +53,116 @@ test("catch the error when a WebError is thrown and update the status code and t
     [httpsRedirectMiddleware(tracer), middleware],
     handler,
   );
+
+  const host = "test.test";
+  const url = "/test?query=string";
   const response = new httpMock.Response();
 
   await wrappedhandler(
     new httpMock.Request({
-      url: "/test",
+      url,
       headers: {
-        host: "http://example.net?query=string",
+        "x-forwarded-proto": "https",
+        host,
       },
     }),
     response,
   );
 
-  expect(response.statusCode).toBe(HttpStatus.PERMANENT_REDIRECT);
-  expect(response.getHeader("Location")).toBe(
-    "https://example.net?query=string",
+  expect(response.statusCode).toBe(HttpStatus.OK);
+});
+
+test("Do not redirect on https when host is localhost", async () => {
+  expect.assertions(1);
+
+  const tracer = new InMemoryTracer();
+  const middleware = errorMiddleware(tracer);
+  const handler = (): void => {
+    response.end("OK");
+  };
+  const wrappedhandler = wrapMiddlewares(
+    [httpsRedirectMiddleware(tracer), middleware],
+    handler,
   );
+
+  const host = "localhost:3000";
+  const url = "/test?query=string";
+  const response = new httpMock.Response();
+
+  await wrappedhandler(
+    new httpMock.Request({
+      url,
+      headers: {
+        "x-forwarded-proto": "https",
+        host,
+      },
+    }),
+    response,
+  );
+
+  expect(response.statusCode).toBe(HttpStatus.OK);
+});
+
+test("Do not redirect when the scheme is https", async () => {
+  expect.assertions(1);
+
+  const tracer = new InMemoryTracer();
+  const middleware = errorMiddleware(tracer);
+  const handler = (): void => {
+    response.end("OK");
+  };
+  const wrappedhandler = wrapMiddlewares(
+    [httpsRedirectMiddleware(tracer), middleware],
+    handler,
+  );
+
+  const host = "test.test";
+  const url = "/test?query=string";
+  const response = new httpMock.Response();
+
+  await wrappedhandler(
+    new httpMock.Request({
+      url,
+      headers: {
+        "x-forwarded-proto": "https",
+        host,
+      },
+    }),
+    response,
+  );
+
+  expect(response.statusCode).toBe(HttpStatus.OK);
+});
+
+test("Transfers the body on redirection", async () => {
+  expect.assertions(2);
+
+  const tracer = new InMemoryTracer();
+  const middleware = errorMiddleware(tracer);
+  const handler = (): void => {
+    response.end("OK");
+  };
+  const wrappedhandler = wrapMiddlewares(
+    [httpsRedirectMiddleware(tracer), middleware],
+    handler,
+  );
+
+  const body = { test: "test" };
+  const host = "test.test";
+  const url = "/test";
+  const response = new httpMock.Response();
+  const request = new httpMock.Request({
+    url,
+    headers: {
+      host,
+    },
+    buffer: Buffer.from(JSON.stringify(body)),
+  });
+
+  await wrappedhandler(request, response);
+
+  const requestBody = JSON.parse(await readBody(request));
+
+  expect(requestBody).toMatchObject(body);
+  expect(response.statusCode).toBe(HttpStatus.PERMANENT_REDIRECT);
 });
