7048cc5c6903f7a8a73f3387b227f4261b47a979
Vincent Billey <vincent.billey@gmail.com>
2022-06-06 12:29:27 +0200

    Bump all dev deps, relax dependabot and remove yarn workspace

--- packages/web/tests/middlewares/middleware-coordination.test.ts
+++ packages/web/tests/middlewares/middleware-coordination.test.ts
@@ -37,28 +37,23 @@ test("logs and catch a WebError correctly", async () => {
   );
   const response = new httpMock.Response();
 
-  try {
-    await wrappedhandler(
-      new httpMock.Request({
-        url: "/test",
-      }),
-      response,
-    );
+  await wrappedhandler(
+    new httpMock.Request({
+      url: "/test",
+    }),
+    response,
+  );
 
-    const log = logger.getLog(0);
-    expect(log).toMatchObject({
-      message: "Error: something wrong happened",
-      path: "/test",
-      remoteaddr: "127.0.0.1",
-      statusCode: HttpStatus.NOT_FOUND,
-      traceid: "1",
-    });
+  const log = logger.getLog(0);
+  expect(log).toMatchObject({
+    message: "Error: something wrong happened",
+    path: "/test",
+    statusCode: HttpStatus.NOT_FOUND,
+    traceid: "1",
+  });
 
-    expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
-    expect(response._internal.buffer.toString()).toBe(JSON.stringify(error));
-  } catch (error) {
-    fail("No error should have been throwned but got:\n" + error.toString());
-  }
+  expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+  expect(response._internal.buffer.toString()).toBe(JSON.stringify(error));
 });
 
 test("logs and catch a random error correctly", async () => {
@@ -80,25 +75,20 @@ test("logs and catch a random error correctly", async () => {
   );
   const response = new httpMock.Response();
 
-  try {
-    await wrappedhandler(
-      new httpMock.Request({
-        url: "/test",
-      }),
-      response,
-    );
+  await wrappedhandler(
+    new httpMock.Request({
+      url: "/test",
+    }),
+    response,
+  );
 
-    const log = logger.getLog(0);
-    expect(log).toMatchObject({
-      message: "Error: unexpected error",
-      path: "/test",
-      remoteaddr: "127.0.0.1",
-      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-      traceid: "1",
-    });
+  const log = logger.getLog(0);
+  expect(log).toMatchObject({
+    message: "Error: unexpected error",
+    path: "/test",
+    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+    traceid: "1",
+  });
 
-    expect(response.statusCode).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-  } catch (error) {
-    fail("No error should have been throwned but got:\n" + error.toString());
-  }
+  expect(response.statusCode).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
 });
